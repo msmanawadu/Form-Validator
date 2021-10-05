1d51a59aba7913b7907591233983e0e80251b228
msmanawadu <msmanawadu@gmail.com>
2021-10-05 17:14:09 +0530

    Implementing Check Length, Email & Password Match

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// pull in all the needed DOM elements
+// Pull in all the needed DOM elements
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -7,7 +7,9 @@ const password2 = document.getElementById('password2');
 
 // Show input error message
 function showError(input, message) {
+    // Tap into the input element's parent
     const formControl = input.parentElement;
+
     formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
     small.innerText = message;
@@ -20,19 +22,22 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
+    // Email Regex
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
 
 }
 
-// Get field name
+// Get field name and Uppercase only the first letter
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
-
-
-
 
 // Check Required Fields
 function checkRequired(inputArray) {
@@ -46,16 +51,31 @@ function checkRequired(inputArray) {
     })
 }
 
+// Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
 
+// Check Passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
+}
 
-
-
-
-
-// Submit event on the form
+// Event listener for Submit event on the form
 form.addEventListener('submit', function (e) {
 
     e.preventDefault();
-
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
