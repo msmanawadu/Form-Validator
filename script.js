197414049c74e6a3bcebdf7d4d12392ee679a055
msmanawadu <msmanawadu@gmail.com>
2021-10-05 14:13:15 +0530

    Check Required and Refactoring the code

--- script.js
+++ script.js
@@ -26,6 +26,26 @@ function isValidEmail(email) {
 
 }
 
+// Get field name
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+
+
+// Check Required Fields
+function checkRequired(inputArray) {
+    inputArray.forEach(function (input) {
+
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    })
+}
+
 
 
 
@@ -37,29 +57,5 @@ form.addEventListener('submit', function (e) {
 
     e.preventDefault();
 
-    if (username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-    }
-
-    if (email.value === '') {
-        showError(email, 'Email is required');
-    } else if (!isValidEmail(email.value)) {
-        showError(email, 'Email is not vaid');
-    } else {
-        showSuccess(email);
-    }
-
-    if (password.value === '') {
-        showError(password, 'Password is required');
-    } else {
-        showSuccess(password);
-    }
-
-    if (password2.value === '') {
-        showError(password2, 'Password 2 is required');
-    } else {
-        showSuccess(password2);
-    }
+    checkRequired([username, email, password, password2]);
 });
